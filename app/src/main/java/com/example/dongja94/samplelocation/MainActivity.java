d4ae9447375268824225e31f76e54f81abb37ea6
dongja94 <dongja94@gmail.com>
2015-10-23 17:53:44 +0900

    Geocoder

--- app/src/main/java/com/example/dongja94/samplelocation/MainActivity.java
+++ app/src/main/java/com/example/dongja94/samplelocation/MainActivity.java
@@ -4,28 +4,62 @@ import android.Manifest;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Criteria;
+import android.location.Geocoder;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
+import android.location.LocationProvider;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.os.Message;
 import android.provider.Settings;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.text.TextUtils;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import java.io.IOException;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
     LocationManager mLM;
     String mProvider;
     TextView locationView;
+
+    public static final int MESSAGE_LOCATION_TIMEOUT = 1;
+    public static final int TIMEOUT_INTERVAL = 60000;
+
+    Handler mHandler = new Handler(Looper.getMainLooper()) {
+        @Override
+        public void handleMessage(Message msg) {
+            super.handleMessage(msg);
+            switch (msg.what) {
+                case MESSAGE_LOCATION_TIMEOUT:
+                    // Timeout...
+                    break;
+            }
+        }
+    };
+
+    ListView listView;
+    ArrayAdapter<Address> mAdapter;
+    EditText keywordView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,20 +77,75 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        mLM = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
-        mProvider = LocationManager.GPS_PROVIDER;
         locationView = (TextView) findViewById(R.id.text_location);
+        listView = (ListView) findViewById(R.id.listView);
+        keywordView = (EditText)findViewById(R.id.edit_keyword);
+        mAdapter = new ArrayAdapter<Address>(this, android.R.layout.simple_list_item_1);
+        listView.setAdapter(mAdapter);
+
+        Button btn = (Button)findViewById(R.id.btn_search);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String keyword = keywordView.getText().toString();
+                if (!TextUtils.isEmpty(keyword)) {
+                    if (Geocoder.isPresent()) {
+                        Geocoder coder = new Geocoder(MainActivity.this);
+                        try {
+                            List<Address> list = coder.getFromLocationName(keyword,10);
+                            mAdapter.clear();
+                            for (Address addr : list) {
+                                mAdapter.add(addr);
+                            }
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }
+            }
+        });
+        mLM = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        Criteria criteria = new Criteria();
+        criteria.setAccuracy(Criteria.ACCURACY_COARSE);
+        criteria.setCostAllowed(true);
+        mProvider = mLM.getBestProvider(criteria, true);
+        if (mProvider == null) {
+            mProvider = LocationManager.GPS_PROVIDER;
+        }
     }
 
     LocationListener mListener = new LocationListener() {
         @Override
         public void onLocationChanged(Location location) {
+            mHandler.removeMessages(MESSAGE_LOCATION_TIMEOUT);
             locationView.setText(location.getLatitude() + "," + location.getLongitude());
+            if (Geocoder.isPresent()) {
+                Geocoder coder = new Geocoder(MainActivity.this);
+                try {
+                    List<Address> list = coder.getFromLocation(location.getLatitude(), location.getLongitude(), 10);
+                    mAdapter.clear();
+                    for (Address addr  : list) {
+                        mAdapter.add(addr);
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
         }
 
         @Override
         public void onStatusChanged(String provider, int status, Bundle extras) {
-
+            if (provider.equals(LocationManager.GPS_PROVIDER)) {
+                switch (status) {
+                    case LocationProvider.OUT_OF_SERVICE:
+                    case LocationProvider.TEMPORARILY_UNAVAILABLE:
+                        // Network Provider change....
+                        break;
+                    case LocationProvider.AVAILABLE:
+                        // Gps Provider change...
+                        break;
+                }
+            }
         }
 
         @Override
@@ -86,15 +175,19 @@ public class MainActivity extends AppCompatActivity {
             }
             return;
         }
+
         if (Build.VERSION.SDK_INT >= 23 && ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED &&
                 ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
             return;
         }
-        Location location = mLM.getLastKnownLocation(mProvider);
-        if (location != null) {
-            mListener.onLocationChanged(location);
-        }
-        mLM.requestLocationUpdates(mProvider, 0, 0, mListener);
+
+        mLM.requestSingleUpdate(mProvider, mListener, null);
+//        Location location = mLM.getLastKnownLocation(mProvider);
+//        if (location != null) {
+//            mListener.onLocationChanged(location);
+//        }
+//        mLM.requestLocationUpdates(mProvider, 0, 0, mListener);
+        mHandler.sendEmptyMessageDelayed(MESSAGE_LOCATION_TIMEOUT, TIMEOUT_INTERVAL);
     }
 
     @Override
