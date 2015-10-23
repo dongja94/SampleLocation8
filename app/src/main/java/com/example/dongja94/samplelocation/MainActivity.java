d9458e3d0368ce3b0160173ce848f35631674e85
dongja94 <dongja94@gmail.com>
2015-10-23 17:09:42 +0900

    Initial commit

---
+++ app/src/main/java/com/example/dongja94/samplelocation/MainActivity.java
@@ -0,0 +1,112 @@
+package com.example.dongja94.samplelocation;
+
+import android.content.Context;
+import android.content.Intent;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.provider.Settings;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    LocationManager mLM;
+    String mProvider;
+    TextView locationView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+        mLM = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        mProvider = LocationManager.GPS_PROVIDER;
+        locationView = (TextView)findViewById(R.id.text_location);
+    }
+
+    LocationListener mListener = new LocationListener() {
+        @Override
+        public void onLocationChanged(Location location) {
+            locationView.setText(location.getLatitude() + "," + location.getLongitude());
+        }
+
+        @Override
+        public void onStatusChanged(String provider, int status, Bundle extras) {
+
+        }
+
+        @Override
+        public void onProviderEnabled(String provider) {
+
+        }
+
+        @Override
+        public void onProviderDisabled(String provider) {
+
+        }
+    };
+
+    boolean isFirst = true;
+    @Override
+    protected void onStart() {
+        super.onStart();
+        if (!mLM.isProviderEnabled(mProvider)) {
+            if (isFirst) {
+                Intent intent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                startActivity(intent);
+                isFirst = false;
+            } else {
+                Toast.makeText(this, "This is ...." , Toast.LENGTH_SHORT).show();
+                finish();
+            }
+            return;
+        }
+        Location location = mLM.getLastKnownLocation(mProvider);
+        if (location != null) {
+            mListener.onLocationChanged(location);
+        }
+        mLM.requestLocationUpdates(mProvider, 0, 0, mListener);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
