2c243b16746608e5ac48cd5083c4c02f767f406f
dongja94 <dongja94@gmail.com>
2015-10-23 17:19:18 +0900

    checkSelfPermission

--- app/src/main/java/com/example/dongja94/samplelocation/MainActivity.java
+++ app/src/main/java/com/example/dongja94/samplelocation/MainActivity.java
@@ -1,14 +1,18 @@
 package com.example.dongja94.samplelocation;
 
+import android.Manifest;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.provider.Settings;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
@@ -41,7 +45,7 @@ public class MainActivity extends AppCompatActivity {
 
         mLM = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
         mProvider = LocationManager.GPS_PROVIDER;
-        locationView = (TextView)findViewById(R.id.text_location);
+        locationView = (TextView) findViewById(R.id.text_location);
     }
 
     LocationListener mListener = new LocationListener() {
@@ -67,6 +71,7 @@ public class MainActivity extends AppCompatActivity {
     };
 
     boolean isFirst = true;
+
     @Override
     protected void onStart() {
         super.onStart();
@@ -76,9 +81,13 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
                 isFirst = false;
             } else {
-                Toast.makeText(this, "This is ...." , Toast.LENGTH_SHORT).show();
+                Toast.makeText(this, "This is ....", Toast.LENGTH_SHORT).show();
                 finish();
             }
+            return;
+        }
+        if (Build.VERSION.SDK_INT >= 23 && ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED &&
+                ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
             return;
         }
         Location location = mLM.getLastKnownLocation(mProvider);
